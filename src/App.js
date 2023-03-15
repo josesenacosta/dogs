3f9a435a200783edda2fe9744f1a245b1c77a84e
jsena95 <junior.senna95@live.com>
2023-03-15 15:38:44 -0300

    Envio de arquivos para o gitHub

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
+
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        <BrowserRouter>
+          <UserStorage>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <Route
+                path="conta/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+            <Footer />
+          </UserStorage>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Footer.module.css';
+const Footer = () => {
+  return (
+    <>
+      <div className={styles.fooer}>footer</div>
+    </>
+  );
+};
+
+export default Footer;
