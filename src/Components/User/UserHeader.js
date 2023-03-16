8264c4f0ccecd18cdaf7b2a40c0d478fadd8cb87
jsena95 <junior.senna95@live.com>
2023-03-16 13:11:03 -0300

    Configurando o Header da pagina de usuario

---
+++ src/Components/User/UserHeader.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import UserHeaderNav from './UserHeaderNav';
+import styles from './UserHeader.module.css';
+import { useLocation } from 'react-router-dom';
+
+const UserHeader = () => {
+  const [title, setTitle] = React.useState('');
+  const location = useLocation();
+
+  React.useEffect(() => {
+    setTitle(location.pathname);
+    const { pathname } = location;
+    switch (pathname) {
+      case '/conta/postar':
+        setTitle('Poste Sua Foto');
+        break;
+      case '/conta/estatisticas':
+        setTitle('Estatísticas');
+        break;
+      default:
+        setTitle('Minha Conta');
+    }
+  }, [location]);
+
+  return (
+    <header className={styles.header}>
+      <h1 className="title">{title}</h1>
+      <UserHeaderNav />
+    </header>
+  );
+};
+
+export default UserHeader;
---
+++ src/Components/User/UserPhotoPost.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserPhotoPost = () => {
+  return <div>photopost</div>;
+};
+
+export default UserPhotoPost;
