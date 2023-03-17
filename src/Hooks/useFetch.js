e749488c55003f7a505f38a4e96bf657dc19836d
jsena95 <junior.senna95@live.com>
2023-03-17 15:45:43 -0300

    Incluindo funcao de comentario no modal

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -13,6 +13,7 @@ const useFetch = () => {
       setLoading(true);
       response = await fetch(url, options);
       json = await response.json();
+      console.log(response);
       if (response.ok === false) throw new Error(json.message);
     } catch (err) {
       json = null;
