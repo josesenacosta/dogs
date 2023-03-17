7df9aa949cc9931df9d9a933cf491ca787b21087
jsena95 <junior.senna95@live.com>
2023-03-17 11:03:25 -0300

    configurando modal de photo

--- src/Components/Feed/FeedModal.js
+++ src/Components/Feed/FeedModal.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import useFetch from '../../Hooks/useFetch';
+import styles from './FeedModal.module.css';
+import { PHOTO_GET } from '../api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import PhotoContent from '../Photo/PhotoContent';
 
-const FeedModal = () => {
-  return <div></div>;
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  return (
+    <div className={styles.modal}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;
