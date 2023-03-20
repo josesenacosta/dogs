f5d95b624e6e210d211d40486856c134d48c1a74
jsena95 <junior.senna95@live.com>
2023-03-20 14:38:39 -0300

    corregindo carregamento de pagina ao deletar foto

--- src/Components/Helper/ProtectedRoute.js
+++ src/Components/Helper/ProtectedRoute.js
@@ -4,8 +4,14 @@ import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
   const { login } = React.useContext(UserContext);
-
-  return login ? children : <Navigate to="/login" />;
+  if (login === true) {
+    return children;
+  } else if (login === false) {
+    return <Navigate to="/login" />;
+  } else {
+    return;
+  }
+  //return login ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
